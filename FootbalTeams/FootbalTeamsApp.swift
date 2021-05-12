151ca4556864727c1c269069c9501f6e52a54b2f
gusadi09 <gusadi872@gmail.com>
2021-05-12 18:10:19 +0800

    Initial Commit

---
+++ FootbalTeams/FootbalTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootbalTeamsApp.swift
+//  FootbalTeams
+//
+//  Created by Gus Adi on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FootbalTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
